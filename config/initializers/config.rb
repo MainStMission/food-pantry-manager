8c2131cfec24ec62508fae047f130cfdadb7d970
Shane Emmons <shane@teamsnap.com>
2012-09-27 14:34:59 -0400

    shane, always test to make sure there's no typos...geez

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -3,8 +3,8 @@
 require 'fileutils'
 
 unless Rails.env.production?
-  config_file = Rails.root.join '/config/config.yml'
-  FileUtils.cp((config_file.to_s + '.example'), config_file) unless config_file.exist?
+  config_file = Rails.root.join("config/config.yml")
+  FileUtils.cp((config_file.to_s + ".example"), config_file) unless config_file.exist?
 
   config = YAML.load_file(config_file)[Rails.env]
 
