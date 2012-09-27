143f20c1e906d0bf395074171938a5bfb8d29b85
Shane Emmons <shane@teamsnap.com>
2012-09-27 14:32:38 -0400

    simplify config.rb

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -3,10 +3,8 @@
 require 'fileutils'
 
 unless Rails.env.production?
-  config_file = File.expand_path(File.join(Rails.root, '/config/config.yml'))
-  config_file_sample = File.expand_path(File.join(Rails.root, '/config/config.yml.sample'))
-
-  FileUtils.cp(config_file_sample, config_file) unless File.exists?(config_file)
+  config_file = Rails.root.join '/config/config.yml'
+  FileUtils.cp((config_file.to_s + '.example'), config_file) unless config_file.exist?
 
   config = YAML.load_file(config_file)[Rails.env]
 
