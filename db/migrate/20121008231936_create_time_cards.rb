5b851c085fb96d7d454d43eeda9069e25be9c1e3
Corey Purcell <corey.purcell@gmail.com>
2012-10-08 19:37:42 -0400

    add time cards for tracking hours

---
+++ db/migrate/20121008231936_create_time_cards.rb
@@ -0,0 +1,13 @@
+class CreateTimeCards < ActiveRecord::Migration
+  def up
+    create_table :time_cards do |t|
+      t.references :volunteer
+      t.datetime :start_time
+      t.datetime :end_time
+    end
+  end
+
+  def down
+    drop_table :time_cards
+  end
+end
