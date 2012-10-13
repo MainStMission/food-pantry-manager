e92f854f22f2d7010f300b4c44affcd9005a961f
Corey Purcell <corey.purcell@gmail.com>
2012-10-13 17:17:29 -0400

    add donation feature and generalize the managing cucumber steps

--- app/models/donation.rb
+++ app/models/donation.rb
@@ -2,6 +2,9 @@ class Donation < ActiveRecord::Base
   belongs_to :donor
   attr_accessible :weight, :donor_id
 
-  delegate :name, to: :donor, prefix: true
+  # delegate :name, to: :donor, prefix: true
+  def donor_name
+    donor.name if donor
+  end
 
 end
