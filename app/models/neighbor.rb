c0f9ce3a55f2e1a752eafa3bb27f6d008c6a685e
Tom Brooke <tom.brooke@gmail.com>
2012-10-14 17:18:25 -0400

    start on neighbor views

--- app/models/neighbor.rb
+++ app/models/neighbor.rb
@@ -1,6 +1,13 @@
-class Neighbor < ActiveRecord::Base   attr_accessible :car_payment, :city,
-:close_date, :date_of_proof, :first_name, :food_stamps, :last_name,
-:monthly_income, :number_of_children, :open_date, :phone,
-:proof_of_residency_type, :rent, :residency_date, :residency_type, :smokes,
-:spouse, :ss_numb, :state, :street, :utilities, :zip 
+class Neighbor < ActiveRecord::Base  
+
+  attr_accessible :car_payment, :city,
+   :close_date, :date_of_proof, :first_name, :food_stamps, :last_name,
+   :monthly_income, :number_of_children, :open_date, :phone,
+   :proof_of_residency_type, :rent, :residency_date, :residency_type, :smokes,
+   :spouse, :ss_numb, :state, :street, :utilities, :zip 
+
+   def name
+     "#{first_name} #{last_name}"
+   end
+   
 end
