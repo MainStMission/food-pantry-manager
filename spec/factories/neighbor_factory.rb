e870e64823ec2278c209ada3396108fbabdd926d
tbrooke <tom.brooke@gmail.com>
2013-01-20 15:52:10 -0500

    Redid Specs

--- spec/factories/neighbor_factory.rb
+++ spec/factories/neighbor_factory.rb
@@ -1,9 +1,10 @@
 # -*- encoding : utf-8 -*-
 FactoryGirl.define do
   factory :neighbor do
-    first_name "Tom"
-    middle_name "M"
-    last_name  "Terrific"
-    age "45"
+    first_name 'Tom'
+    middle_name 'M'
+    last_name  'Terrific'
+    email 'tmb@brookelaw.com'
+    age 45
   end
 end
--- spec/models/household_spec.rb
+++ spec/models/household_spec.rb
@@ -1,9 +1,32 @@
-require 'spec_helper'
+# -*- encoding : utf-8 -*-
+require_relative '../spec_helper'
 
 describe Household do
 
-  it { should  have_many(:neighbors)}
+  it { should have_many(:neighbors) }
+
+  it 'has a valid factory' do
+    FactoryGirl.create(:household, household_name: 'Jones').should be_valid
+  end
+
+  it 'should be invalid without a household_name' do
+    #FactoryGirl.build(:household, household_name: nil).should_not be_valid
+  end
+
+  it 'should not allow duplicates' do
+    FactoryGirl.create(:household, household_name: 'Jones')
+    FactoryGirl.build(:household, household_name: 'Jones').should_not be_valid
+  end
+
+  it 'should not allow duplicates - make sure ok if different' do
+    FactoryGirl.create(:household, household_name: 'Jones')
+    FactoryGirl.build(:household, household_name: 'Brooke').should be_valid
+  end
+
+  it 'can have a neighbor' do
+    FactoryGirl.create(:household, household_name: 'Jones').
 
 
+  end
 
 end
