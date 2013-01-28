2d0ba8dd6ddddd67b9558982396865861a17a567
tbrooke <tom.brooke@gmail.com>
2013-01-27 19:08:30 -0500

    Revert "Revert "indexed household""
    This reverts commit 6bc4732f8f1d80dca97d8b9269b5c4becd318b83.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130120141100) do
+ActiveRecord::Schema.define(:version => 20130120205403) do
 
   create_table "donations", :force => true do |t|
     t.integer  "donor_id"
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(:version => 20130120141100) do
     t.datetime "updated_at",     :null => false
     t.string   "name"
   end
+
+  add_index "households", ["household_name"], :name => "index_households_on_household_name", :unique => true
 
   create_table "neighbors", :force => true do |t|
     t.decimal  "monthly_income",          :precision => 8, :scale => 2
