2873cfa31ae33121f548a58aa548a5544a9fd215
tbrooke <tom.brooke@gmail.com>
2013-01-27 19:04:31 -0500

    Revert "modofied household model and added form"
    This reverts commit 00b40ddfcf6e3d2e8cf3de443397999f4e735f7f.

--- db/migrate/20130117134618_add_name_to_houshold.rb
+++
@@ -1,7 +0,0 @@
-class AddNameToHoushold < ActiveRecord::Migration
-  def change
-    add_column :households, :name, :string
-    remove_column :households, :person_id, :integer
-    remove_column :households, :friend_id, :integer
-  end
-end
