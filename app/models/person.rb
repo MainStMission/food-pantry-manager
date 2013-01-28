0ef4feb43020bd746092b00f7d29737c046d3bb2
tbrooke <tom.brooke@gmail.com>
2013-01-27 19:58:47 -0500

    removed old remnants of person relationship

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,4 +1,10 @@
 class Person < ActiveRecord::Base
 
+<<<<<<< HEAD
+=======
+ 
+ 
+ 
+>>>>>>> removed old remnants of person relationship
   attr_accessible :address, :age, :city, :email, :first_name, :last_name, :middle_name, :phone, :state, :title, :zip
 end
