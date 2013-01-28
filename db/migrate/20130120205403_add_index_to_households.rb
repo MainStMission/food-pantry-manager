6bc4732f8f1d80dca97d8b9269b5c4becd318b83
tbrooke <tom.brooke@gmail.com>
2013-01-27 19:04:31 -0500

    Revert "indexed household"
    This reverts commit 22d3a051e4636f8055b6259a1d4e8dd6c89fd751.

--- db/migrate/20130120205403_add_index_to_households.rb
+++
@@ -1,5 +0,0 @@
-class AddIndexToHouseholds < ActiveRecord::Migration
-  def change
-    add_index :households, :household_name , unique: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130120205403) do
+ActiveRecord::Schema.define(:version => 20130120141100) do
 
   create_table "donations", :force => true do |t|
     t.integer  "donor_id"
@@ -34,8 +34,6 @@ ActiveRecord::Schema.define(:version => 20130120205403) do
     t.datetime "updated_at",     :null => false
     t.string   "name"
   end
-
-  add_index "households", ["household_name"], :name => "index_households_on_household_name", :unique => true
 
   create_table "neighbors", :force => true do |t|
     t.decimal  "monthly_income",          :precision => 8, :scale => 2
