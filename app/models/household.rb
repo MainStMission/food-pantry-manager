6a41f7217956c5162301c5189f5ba55fa9a9485e
tbrooke <tom.brooke@gmail.com>
2013-01-27 19:08:30 -0500

    Revert "created primary household"
    This reverts commit 9b03ac9af3d0729eebb2b7027bfb4f8379701fbe.

--- app/models/household.rb
+++ app/models/household.rb
@@ -1,6 +1,7 @@
 class Household < ActiveRecord::Base
 
-  has_many :neighbors
+  belongs_to :neighbor
+  belongs_to :relation, :class_name => 'Neighbor'
 
   attr_accessible :friend_id, :person_id, :relationship
 end
--- app/models/neighbor.rb
+++ app/models/neighbor.rb
@@ -1,11 +1,14 @@
 # -*- encoding : utf-8 -*-
-class Neighbor < ActiveRecord::Base
-
+class Neighbor < ActiveRecord::Base  
   include ActiveModel::ForbiddenAttributesProtection
 
   has_many :visits
 
-  belongs_to :household
+  has_many :households
+  has_many :relations, :through => :households
+  has_many :inverse_relationships, :class_name => 'Household', :foreign_key => 'friend_id'
+  has_many :inverse_relations, :through => :inverse_relationships, :source => :person
+
 
   attr_encrypted :ssn, :key => Rails.application.config.secret_token
 
