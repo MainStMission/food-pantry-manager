cc4e4d3e6ac68a563e0975115ae51c07d9f2913b
tbrooke <tom.brooke@gmail.com>
2013-01-27 21:10:30 -0500

    cleanup

--- spec/models/household_spec.rb
+++ spec/models/household_spec.rb
@@ -1,9 +1,5 @@
 require 'spec_helper'
 
-<<<<<<< HEAD
 describe Household do
   pending "add some examples to (or delete) #{__FILE__}"
 end
-=======
-
->>>>>>> removed old remnants of person relationship
