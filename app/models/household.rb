5d329705b5105cd7be435795f312a2bc4a157b57
tbrooke <tom.brooke@gmail.com>
2013-01-27 19:08:30 -0500

    Revert "Added validations"
    This reverts commit a8674945b7d48668121a8f8a608b443f58d4439b.

--- app/models/household.rb
+++ app/models/household.rb
@@ -1,15 +1,9 @@
 class Household < ActiveRecord::Base
   include ActiveModel::ForbiddenAttributesProtection
 
-  validates :household_name, presence: true, uniqueness: true
-
   has_many :neighbors
   attr_accessible :household_name
   accepts_nested_attributes_for :neighbors , :reject_if => :all_blank, :allow_destroy => true
-
-  def add_neighbor
-    self.neighbor.name if neighbor
-  end
 
   def neighbor_name
     Neighbor.find(params[:id])
