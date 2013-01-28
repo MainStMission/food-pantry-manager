6ea6f2fa7d29d785d669ddd93acaab5d89e3ba0e
tbrooke <tom.brooke@gmail.com>
2013-01-27 19:04:31 -0500

    Revert "Redid Specs"
    This reverts commit e870e64823ec2278c209ada3396108fbabdd926d.

--- spec/models/neighbor_spec.rb
+++ spec/models/neighbor_spec.rb
@@ -7,13 +7,6 @@ describe Neighbor do
 
   it { should belong_to(:household) }
 
-  it 'has a valid factory' do
-    FactoryGirl.create(:neighbor).should be_valid
-  end
-
-  it 'should be invalid without a last_name' do
-    FactoryGirl.build(:neighbor, last_name: nil).should_not be_valid
-  end
 
   context 'ssn' do
     let(:neighbor) do
