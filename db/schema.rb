b024a986f5e384cca0d200782f327c4b48f79546
tbrooke <tom.brooke@gmail.com>
2013-02-28 14:31:04 -0500

    updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(:version => 20130219013031) do
-=======
 ActiveRecord::Schema.define(:version => 20130225024800) do
->>>>>>> nested_attributes
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
