13ad9007323bbb1240cd54852366acc7a0e04b14
tbrooke <tom.brooke@gmail.com>
2013-03-05 09:27:44 -0500

    finished migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130225024800) do
+ActiveRecord::Schema.define(:version => 20130305142203) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -169,6 +169,7 @@ ActiveRecord::Schema.define(:version => 20130225024800) do
     t.string   "status"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+    t.string   "group"
   end
 
 end
