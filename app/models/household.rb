950e0aa4e69028dfb6ec1a1e253edbcb9b5147aa
tbrooke <tom.brooke@gmail.com>
2013-04-12 17:09:07 -0400

    Set up delegation for households

--- app/models/household.rb
+++ app/models/household.rb
@@ -6,6 +6,7 @@ class Household < ActiveRecord::Base
  validates :household_name, uniqueness: true
 
  has_many :neighbors
+ has_many :visits
  accepts_nested_attributes_for :neighbors, allow_destroy: true, reject_if: :all_blank
 
 end
--- app/models/visit.rb
+++ app/models/visit.rb
@@ -3,8 +3,10 @@ class Visit < ActiveRecord::Base
   include ActiveModel::ForbiddenAttributesProtection
 
   belongs_to :neighbor
+  belongs_to :household
 
   delegate :name, to: :neighbor, prefix: true, allow_nil: true
+  delegate :household_name, to: :household, prefix: true, allow_nil: true
 
   def show_neighbor
     neighbor.last_name if neighbor
