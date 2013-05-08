ac57de131f785fd3d322ae8374c258a9b5ee654d
tbrooke <tom.brooke@gmail.com>
2013-05-07 21:23:56 -0400

    rails_admin migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130505192937) do
+ActiveRecord::Schema.define(:version => 20130508012208) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "resource_id",   :null => false
@@ -125,6 +125,19 @@ ActiveRecord::Schema.define(:version => 20130505192937) do
     t.boolean  "done"
     t.datetime "birth_date"
   end
+
+  create_table "rails_admin_histories", :force => true do |t|
+    t.text     "message"
+    t.string   "username"
+    t.integer  "item"
+    t.string   "table"
+    t.integer  "month",      :limit => 2
+    t.integer  "year",       :limit => 8
+    t.datetime "created_at",              :null => false
+    t.datetime "updated_at",              :null => false
+  end
+
+  add_index "rails_admin_histories", ["item", "table", "month", "year"], :name => "index_rails_admin_histories"
 
   create_table "time_cards", :force => true do |t|
     t.integer  "volunteer_id"
