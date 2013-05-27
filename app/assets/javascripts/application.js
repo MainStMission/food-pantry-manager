f1dd340460f6938bd40d33942c8f22876a0bafc7
tbrooke <tom.brooke@gmail.com>
2013-05-27 09:27:31 -0400

    added custom date picker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -26,3 +26,9 @@
 
 
 
+$(document).on 'pageChanged', ->
+
+    $(".custom_datepicker_selector").datepicker().on 'changeDate', (en) ->
+    correct_format = en.date.getFullYear() + '-' + ('0' + (en.date.getMonth() + 1)).slice(-2) + '-' + ('0' + en.date.getDate()).slice(-2) # date format yyyy-mm-dd
+$(this).parent().find("input[type=hidden]").val(correct_format)
+
