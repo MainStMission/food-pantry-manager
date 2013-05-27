ea658da908762c89458e13df708f5d02e893c6b3
tbrooke <tom.brooke@gmail.com>
2013-05-27 11:06:45 -0400

    removed custom date picker

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -20,15 +20,7 @@
  //= require jquery.datetimeentry
  //= require rails.validations
  //= require rails.validations.simple_form
- //= require bootstrap-datepicker
  //= require date
  //= require_tree .
 
-
-
-$(document).on 'pageChanged', ->
-
-    $(".custom_datepicker_selector").datepicker().on 'changeDate', (en) ->
-    correct_format = en.date.getFullYear() + '-' + ('0' + (en.date.getMonth() + 1)).slice(-2) + '-' + ('0' + en.date.getDate()).slice(-2) # date format yyyy-mm-dd
-$(this).parent().find("input[type=hidden]").val(correct_format)
 
