e3b28ffdf158e3c7bd38c9cf0362abec75eab39a
tbrooke <tom.brooke@gmail.com>
2013-06-08 13:55:57 -0400

    added database example yml to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -15,6 +15,14 @@ set :port, 25000
 set :user, "deployer"
 set :use_sudo, false
 
+task :setup_config, roles: :app do
+  #sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+  #sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
+  run "mkdir -p #{shared_path}/config"
+  put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
+  puts "Now edit the config files in #{shared_path}."
+end
+after "deploy:setup", "deploy:setup_config"
 
 require 'simple-capistrano-unicorn'
 after :deploy, "unicorn:restart"
