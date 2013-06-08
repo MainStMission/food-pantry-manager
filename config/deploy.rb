085daae11bd9a736aade61874386618accf3316e
tbrooke <tom.brooke@gmail.com>
2013-06-08 15:25:38 -0400

    deploy from tbrooke

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
 set :application, "pantry"
-set :repository,  "git@github.com:MainStMission/food-pantry-manager.git"
+set :repository,  "git@github.com:tbrooke/food-pantry-manager.git"
 
 # set :scm, :git # You can set :scm explicitly or Capistrano will make an intelligent guess based on known version control directory names
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
