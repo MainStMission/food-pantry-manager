b8000925f038cf8d2749b4710df9df70689b09d8
tbrooke <tom.brooke@gmail.com>
2013-06-09 18:20:14 -0400

    changed symlink for db

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,7 +36,7 @@ after 'deploy:update_code', 'deploy:symlink_db'
 namespace :deploy do
   desc "Symlinks the database.yml"
   task :symlink_db, :roles => :app do
-    run "ln -nfs #{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml"
+    run "ln -nfs #{deploy_to}/shared/config/database.yml current/config/database.yml"
   end
 end
 
