b2baaba0686a65414e485f3a7c6c70a0c05aae99
tbrooke <tom.brooke@gmail.com>
2013-06-10 08:21:04 -0400

    db create task

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,8 @@ role :db,     "192.168.1.30", :primary => true
 
 
 after 'deploy:update_code', 'deploy:symlink_db'
-after "deploy:symlink_db", "deploy:migrate"
+after 'deploy:symlonk_db', 'deploy:db:create'
+after "deploy:db:create", "deploy:migrate"
 
 namespace :deploy do
     desc "Symlinks the database.yml"
