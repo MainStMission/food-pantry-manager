17471077990544afa08aa58dd2ac429f7691bd7f
tbrooke <tom.brooke@gmail.com>
2013-06-30 15:58:52 -0400

    pulled out some javascrpit

--- app/assets/javascripts/household.js
+++ app/assets/javascripts/household.js
@@ -1,15 +1,1 @@
-$('#submit-btn').click(function() { 
-  $.ajax({
-    type: post,
-    url: '/household_end_point',
-    data: household,
-    success: function() {
-      $('#myModal').show();
-    }
-  })
-});
 
-$(document).ready(function() {
-$('#visit-btn').bind('cocoon:after-insert', function() {
-    $('#visit-btn').hide();
-  })};
