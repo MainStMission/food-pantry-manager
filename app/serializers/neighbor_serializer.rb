cde6c68474a767d04e3905b18eed0772a8fa6d88
tbrooke <tom.brooke@gmail.com>
2013-07-06 12:40:17 -0400

    Added neighbor and visit serilizers

--- app/serializers/neighbor_serializer.rb
+++ app/serializers/neighbor_serializer.rb
@@ -1,3 +1,8 @@
 class NeighborSerializer < ActiveModel::Serializer
-  attributes :id
+
+  attributes :id, :city, :close_date, :date_of_proof, :first_name, :middle_name,  :street,
+      :food_stamps, :last_name, :monthly_income, :notes, 
+      :open_date, :phone, :proof_of_residency_type, :email, :rent, :residency_date,
+      :proof_of_residency_type, :smokes, :spouse, :ssn, :encrypted_ssn, :state, :street,
+      :utilities, :zip ,:created_at, :sex, :apt, :updated_at ,:birth_date
 end
