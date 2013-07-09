7485c691516cee6d64dc49913f07454a4d9d2c9b
tbrooke <tom.brooke@gmail.com>
2013-07-09 19:44:22 -0400

    Updated volunteers

---
+++ app/assets/javascripts/household.js
@@ -0,0 +1,32 @@
+document.getElementById("btnPrint").onclick = function() {
+    printElement(document.getElementById("printThis"));
+    printElement(document.getElementById("printThisToo"), true, "<hr />");
+    window.print();
+}
+
+function printElement(elem, append, delimiter) {
+    var domClone = elem.cloneNode(true);
+
+    var $printSection = document.getElementById("printSection");
+
+    if (!$printSection) {
+        var $printSection = document.createElement("div");
+        $printSection.id = "printSection";
+        document.body.appendChild($printSection);
+    }
+
+    if (append !== true) {
+        $printSection.innerHTML = "";
+    }
+
+    else if (append === true) {
+        if (typeof(delimiter) === "string") {
+            $printSection.innerHTML += delimiter;
+        }
+        else if (typeof(delimiter) === "object") {
+            $printSection.appendChlid(delimiter);
+        }
+    }
+
+    $printSection.appendChild(domClone);
+}
