9623fc6fdeb9c18dafd4d9046c6c900ca535f9ea
tbrooke <tom.brooke@gmail.com>
2013-07-11 22:25:22 -0400

    created migration for mission

---
+++ db/migrate/20130712022222_create_mission_table.rb
@@ -0,0 +1,12 @@
+class CreateMissionTable < ActiveRecord::Migration
+  def up
+    create_table :mission do |t|
+        t.text :visit_note_text
+        t.string :visit_note_title
+     end
+
+  end
+
+  def down
+  end
+end
