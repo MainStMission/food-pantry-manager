00f998014e9290dd5851ec407065f5b9aee78555
tbrooke <tom.brooke@gmail.com>
2013-07-12 08:56:53 -0400

    dropped underscore

--- db/migrate/20130712022222_create_mission_table.rb
+++ db/migrate/20130712022222_create_mission_table.rb
@@ -1,8 +1,8 @@
 class CreateMissionTable < ActiveRecord::Migration
   def up
     create_table :mission do |t|
-        t.text :visit_note_text
-        t.string :visit_note_title
+        t.text :visit-note-text
+        t.string :visit-note-title
 
         t.timestamps
      end
