d798f8205c3331764070d14780c5685a28e6d113
tbrooke <tom.brooke@gmail.com>
2013-07-12 08:30:56 -0400

    fixed mission input form and added seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,5 @@ unless User.find_by_email("user2@example.com").present?
   user2 = User.create! :first_name => 'Second', :last_name => 'User', :email => 'user2@example.com', :password => 'please', :password_confirmation => 'please'
   puts 'New user created: ' << user2.name
 end
+
+Mission.create! :visit_note_title => 'Initial Visit Note', :visit_note_text => 'Thank you for helping by volunteering at Mani Street Mission'
