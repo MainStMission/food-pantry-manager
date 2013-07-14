1e51cd2bf28c26f6fc4f9e29b31260e23cbaf577
tbrooke <tom.brooke@gmail.com>
2013-07-14 10:23:17 -0400

    Set up inintial methods

--- app/models/dashboard.rb
+++ app/models/dashboard.rb
@@ -1,18 +1,22 @@
 class Dashboard
 
 def houshold_summary
-	
+	household_count = Households.count 
+
 end
 
 def visit_summary
+	visit_count = Visits.count
 	
 end
 
 def neighbor_summary
+	neighbor_count = Neighbors.count
 	
 end
 
 def donor_summary
+
 	
 end
 
@@ -21,7 +25,7 @@ def donation_summary
 end
 
 def volunteer_summary
-	
+	volunteer_count = Volunteers.count
 end
 
 
