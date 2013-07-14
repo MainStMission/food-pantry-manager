d4f61ce8a0b82c70bf92fe7bbc38a706400f868e
tbrooke <tom.brooke@gmail.com>
2013-07-14 10:20:20 -0400

    Added Dashboard model

---
+++ app/models/dashboard.rb
@@ -0,0 +1,30 @@
+class Dashboard
+
+def houshold_summary
+	
+end
+
+def visit_summary
+	
+end
+
+def neighbor_summary
+	
+end
+
+def donor_summary
+	
+end
+
+def donation_summary
+	
+end
+
+def volunteer_summary
+	
+end
+
+
+
+
+end
