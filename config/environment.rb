cd3e60f51261b43057a8f3be49d31346fe3fc24d
tbrooke <tom.brooke@gmail.com>
2013-07-17 22:13:53 -0400

    set up Permittance

--- config/environment.rb
+++ config/environment.rb
@@ -4,3 +4,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 FoodPantry::Application.initialize!
+
+ActionController::Base.send :include, ActionController::Permittance
