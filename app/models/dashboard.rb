a2c235a7e7cab7b1650dc746f8314649802aa8c5
tbrooke <tom.brooke@gmail.com>
2013-07-21 15:29:52 -0400

    Fixed Dashboard

--- app/models/dashboard.rb
+++ app/models/dashboard.rb
@@ -14,11 +14,18 @@ class Dashboard
 end
 
 def visit_summary
-	visit_count = Visits.count
+	@visit_count = Visit.all.count
 	@month = "#{Date.today.strftime('%B')}"
 	@prev_month = "#{Date.today.prev_month.strftime('%B')}"
-	distinct_visits_this_month = Visits.by_month(@month).select("DISTINCT(household_id)").count
-	distinct_visits_last_month = Visits.by_month(@prev_month).select("DISTINCT(household_id)").count
+	@distinct_visits_this_month = Visit.by_month(@month).select("DISTINCT(household_id)").count
+
+	@distinct_visits_last_month = Visit.by_month(@prev_month).select("DISTINCT(household_id)").count
+
+  {
+      visit_count: @visit_count, this_month: @month, current_visits: @distinct_visits_this_month,
+        prev_month: @prev_month, last_month_visits: @distinct_visits_last_month,
+
+        }
 	
 end
 
