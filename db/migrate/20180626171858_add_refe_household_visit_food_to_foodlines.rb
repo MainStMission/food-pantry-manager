89e6e5662e90bd71cc4cb512716688bad1037273
tbrooke <tom.brooke@gmail.com>
2018-07-23 08:12:58 -0400

    further fix to migrations

--- db/migrate/20180626171858_add_refe_household_visit_food_to_foodlines.rb
+++
@@ -1,7 +0,0 @@
-class AddRefeHouseholdVisitFoodToFoodlines < ActiveRecord::Migration
-  def change
-    add_column :foodlines, :household_id, :integer
-    add_column :foodlines, :visit_id, :integer
-    add_column :foodlines, :food_id, :integer
-  end
-end
