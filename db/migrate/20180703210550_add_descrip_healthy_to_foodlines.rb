74a95940c5de3468c5d51fb211dab457dc0723f8
tbrooke <tom.brooke@gmail.com>
2018-07-28 23:32:57 -0400

    deleted foodlines reference for travis

--- db/migrate/20180703210550_add_descrip_healthy_to_foodlines.rb
+++
@@ -1,6 +0,0 @@
-class AddDescripHealthyToFoodlines < ActiveRecord::Migration
-  def change
-    add_column :foodlines, :description, :string
-    add_column :foodlines, :healthy, :string
-  end
-end
